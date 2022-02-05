229da57f300419957d6630980b89ca0f49da3795
枫叶秋林 <80571808+liuqianpan2008@users.noreply.github.com>
2022-02-05 15:37:31 +0800

    新增 简单奖杯榜

--- src/main/java/org/Command/medal.java
+++ src/main/java/org/Command/medal.java
@@ -3,6 +3,7 @@ package org.Command;
 import net.mamoe.mirai.console.command.CommandSender;
 import net.mamoe.mirai.console.command.java.JCompositeCommand;
 import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.message.data.At;
 import net.mamoe.mirai.message.data.ForwardMessageBuilder;
 import net.mamoe.mirai.message.data.PlainText;
 import net.mamoe.mirai.utils.ExternalResource;
@@ -25,9 +26,13 @@ public class medal extends JCompositeCommand {
         setPrefixOptional(true);
     }
     private RemoteWebDriverConfig config = RemoteWebDriverConfig.INSTANCE;
-
-    @SubCommand("当前赛程")
-    public void onCommand(CommandSender sender) {
+    @SubCommand("今日赛程")
+//    choice 0为开始比赛项目，1为正在进行项目，2今天已完成项目
+    public void onCommand(CommandSender sender,int choice) {
+        if (choice>2 || choice<0){
+            sender.sendMessage("选择参数错误");
+            return;
+        }
         Runnable runnable=()->{
             sender.sendMessage("正在截图。。。请稍等");
             RemoteWebDriver driver = MiraiSeleniumPlugin.INSTANCE.driver(config);
@@ -44,14 +49,25 @@ public class medal extends JCompositeCommand {
             }
             sender.sendMessage("访问网站成功，截图开始！");
             try {
+                ForwardMessageBuilder seedbuilder = new ForwardMessageBuilder(sender.getSubject());
                 List<WebElement> info = driver.findElements(By.cssSelector("#cardList li"));
+                String choiceS="";
+                if (choice==0){
+                     choiceS="未开始";
+                }else if (choice==1){
+                     choiceS="比赛中";
+                }else if (choice==2) {
+                     choiceS="已结束";
+                }
+                String finalChoiceS = choiceS;
                 info.forEach(v->{
                     String a = v.findElement(new By.ByClassName("cardList_content")).findElements(By.tagName("div")).get(1).findElements(By.tagName("div")).get(1).getText();
-                    if (a.equals("比赛中")){
+                    if (a.equals(finalChoiceS)){
                         File screenshot = v.getScreenshotAs(OutputType.FILE);
                         ExternalResource res = ExternalResource.create(screenshot);
                         net.mamoe.mirai.message.data.Image image1 =  Contact.uploadImage(sender.getSubject(),res);
-                        sender.sendMessage(image1);
+                        seedbuilder.add(sender.getBot(),image1);
+//                        sender.sendMessage(image1);
                         try {
                             res.close();
                         } catch (IOException e) {
@@ -59,13 +75,50 @@ public class medal extends JCompositeCommand {
                         }
                     }
                 });
+                sender.sendMessage(seedbuilder.build());
             } catch (Exception e) {
                 ForwardMessageBuilder builder = new ForwardMessageBuilder(sender.getSubject());
                 builder.add(sender.getBot(),new PlainText(e.getMessage()));
-                sender.sendMessage(builder.build());
+                sender.sendMessage(builder.build().plus(new At(sender.getUser().getId())));
                 e.printStackTrace();
             }
         };
         new Thread(runnable).start();
     }
+    //https://www.beijing2022.cn/
+    @SubCommand("奖牌")
+    public void Mymedal(CommandSender sender) {
+        Runnable runnable=()->{
+            sender.sendMessage("正在截图。。。请稍等");
+            RemoteWebDriver driver = MiraiSeleniumPlugin.INSTANCE.driver(config);
+            driver.get("https://www.beijing2022.cn/");
+            long current=System.currentTimeMillis();
+            while (true){
+                if (SeleniumToolKt.isReady(driver)){
+                    break;
+                }
+                if (current - System.currentTimeMillis()>100000){
+                    sender.sendMessage("网页加载超时");
+                    break;
+                }
+            }
+            sender.sendMessage("访问网站成功，截图开始！");
+            try {
+                ForwardMessageBuilder seedbuilder = new ForwardMessageBuilder(sender.getSubject());
+                WebElement info = driver.findElement(By.className("medals-table"));
+                File screenshot = info.getScreenshotAs(OutputType.FILE);
+                ExternalResource res = ExternalResource.create(screenshot);
+                net.mamoe.mirai.message.data.Image image1 =  Contact.uploadImage(sender.getSubject(),res);
+                sender.sendMessage(image1);
+            } catch (Exception e) {
+                ForwardMessageBuilder builder = new ForwardMessageBuilder(sender.getSubject());
+                builder.add(sender.getBot(),new PlainText(e.getMessage()));
+                sender.sendMessage(builder.build().plus(new At(sender.getUser().getId())));
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+    }
+    //GeneralScheduleTable
+
 }
